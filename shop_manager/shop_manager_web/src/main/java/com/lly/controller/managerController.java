2c0adf21a53c7209ca83da7d33d7827e62db3033
lly <liuliyuan666@aliyun.com>
2019-04-22 14:56:39 +0800

    update product_add.js

--- shop_manager/shop_manager_web/src/main/java/com/lly/controller/managerController.java
+++ shop_manager/shop_manager_web/src/main/java/com/lly/controller/managerController.java
@@ -1,14 +1,24 @@
 package com.lly.controller;
 
+import com.lly.common.EasyUITree;
+import com.lly.service.ProductCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 /*
  * manager页面跳转控制
  *
  */
 @Controller
 public class managerController {
+    @Autowired
+    ProductCategoryService productCategoryService;
     @RequestMapping("/manager/{page}")
     public String getPage(@PathVariable String page){
         return page;
@@ -16,5 +26,11 @@ public class managerController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+    @RequestMapping("/manager/product_add")
+    public String findCategorys(HttpServletRequest request){
+        List<EasyUITree> categorys = productCategoryService.findProductCategoryListByParentId((short)0);
+       request.getSession().setAttribute("categorys",categorys);
+        return "product_add";
     }
 }
