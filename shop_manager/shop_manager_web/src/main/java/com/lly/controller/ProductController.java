0f844558a3b69642e5fb59d6fa810fc7f75524c4
liuliyuan <liuliyuan666@aliyun.com>
2019-04-29 23:56:25 +0800

    update productcontroller

--- shop_manager/shop_manager_web/src/main/java/com/lly/controller/ProductController.java
+++ shop_manager/shop_manager_web/src/main/java/com/lly/controller/ProductController.java
@@ -1,18 +1,25 @@
 package com.lly.controller;
 
+import com.lly.common.EasyGrid;
 import com.lly.common.ResponseJsonResult;
 import com.lly.pojo.Product;
 import com.lly.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ProductController {
     @Autowired
     ProductService productService;
-
-    @RequestMapping("product_save")
+    @RequestMapping("/product/list")
+    @ResponseBody
+    public EasyGrid productList(@RequestParam(value="page",defaultValue = "1" )int page,@RequestParam(value="rows",defaultValue = "10" )int rows){
+        EasyGrid easyGrid = productService.listProduct(page,rows);
+        return easyGrid;
+    }
+    @RequestMapping("/product_save")
     @ResponseBody
     public ResponseJsonResult saveProduct(Product product){
         ResponseJsonResult responseJsonResult = productService.saveProduct(product);
