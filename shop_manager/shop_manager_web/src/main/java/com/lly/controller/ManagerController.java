49d738b4887d6b49b70b0d4ef4c418a8a4a6c98e
lly <liuliyuan666@aliyun.com>
2019-04-29 15:06:17 +0800

    update managerController

--- shop_manager/shop_manager_web/src/main/java/com/lly/controller/ManagerController.java
+++ shop_manager/shop_manager_web/src/main/java/com/lly/controller/ManagerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +20,8 @@ import java.util.List;
 public class ManagerController {
     @Autowired
     ProductCategoryService productCategoryService;
-    @RequestMapping("/manager/{page}")
+    //下面的映射就是Rest风格，参数写在{}中，分别用put、delete、post、get对应增删改查的查询，@PathVariable用于接收并绑定参数
+    @RequestMapping(value = "/manager/{page}",method = RequestMethod.GET)
     public String getPage(@PathVariable String page){
         return page;
     }
