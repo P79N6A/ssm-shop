3e9051eed5e807047edc81245cdbff49f82a43d2
liuliyuan <liuliyuan666@aliyun.com>
2019-04-29 23:40:35 +0800

    update product

--- shop_manager/shop_manager_service/src/main/java/com/lly/service/ProductService.java
+++ shop_manager/shop_manager_service/src/main/java/com/lly/service/ProductService.java
@@ -1,10 +1,11 @@
 package com.lly.service;
 
 
+import com.lly.common.EasyGrid;
 import com.lly.common.ResponseJsonResult;
 import com.lly.pojo.Product;
 
 public interface ProductService {
     ResponseJsonResult saveProduct(Product product);
-//    EasyGrid listProduct(int pages, int rows);
+    EasyGrid listProduct(int pages, int rows);
 }
